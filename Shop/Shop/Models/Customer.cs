7ee281055d822c2b37ae5f67fadb6c28f1b7a9a6
Luis Felipe De La Rosa Marrera <luismarrero_10@hotmail.com>
2019-10-18 20:36:43 -0400

    Creando Instancias. Custormer y Product.

---
+++ Shop/Shop/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿namespace Shop.Models
+{
+    public class Customer
+    {
+        #region Properties
+        public int Id{ get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string AddressReference { get; set; }
+        public string NumberPhone { get; set; }
+        public string Email { get; set; }
+        #endregion
+    }
+}
---
+++ Shop/Shop/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace Shop.Models
+{
+    public class Product
+    {
+        #region Properties
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public string LongDescription { get; set; }
+        public decimal Price { get; set; }
+        public string ImageURL { get; set; }
+        public bool IsProductOfTheWeek { get; set; }
+        public string InStock { get; set; }
+        #endregion
+    }
+}
